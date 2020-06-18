e3e4faa1f8349545b62ac92946a13e4aa30d1474
kemas.khaidar <kemask69@gmail.com>
2020-06-18 11:18:29 +0700

    remove duplicate import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,6 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
-import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
