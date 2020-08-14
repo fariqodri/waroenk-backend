893706a554ca28eb314eaeacd40f24cdb9fd3b1f
kemas.khaidar <kemask69@gmail.com>
2020-08-14 18:31:37 +0700

    done bulk upload user seller and product

--- src/misc/services/misc.service.ts
+++ src/misc/services/misc.service.ts
@@ -3,8 +3,31 @@ import { ResponseListBody, ResponseBody } from '../../utils/response';
 import { FaqRepository } from '../repositories/faq.repository';
 import { FaqQuery, LocationQuery } from '../dto/faq.dto';
 import { LocationRepository } from '../repositories/location.repository';
-import { CsvParser } from 'nest-csv-parser'
-import fs from 'fs';
+import { CsvParser } from 'nest-csv-parser';
+import { UserRepository } from '../../users/repositories/users.repository';
+import { SellerAttributeRepository } from '../../users/repositories/seller.repository';
+import { ProductRepository } from '../../products/repositories/product.repository';
+import { UserEntity } from '../../users/entities/users.entity';
+import { nanoid } from 'nanoid';
+import * as bcrypt from 'bcrypt'
+import { SALT_ROUNDS } from '../../constants';
+import { SellerAttribute } from '../../users/entities/seller.entity';
+import { ProductEntity } from '../../products/entities/product.entity';
+import { CategoryRepository } from '../../products/repositories/category.repository';
+
+const { Readable } = require('stream');
+
+function bufferToStream(binary) {
+
+    const readableInstanceStream = new Readable({
+      read() {
+        this.push(binary);
+        this.push(null);
+      }
+    });
+
+    return readableInstanceStream;
+}
 
 class UserParsed {
   full_name: string
@@ -12,18 +35,111 @@ class UserParsed {
   phone: string
 }
 
+class SellerParsed {
+  email: string
+  shop_name: string
+  shop_address: string
+  description: string
+  birth_date: string
+  birth_place: string
+  gender: string
+  image: string
+}
+
+class ProductParsed {
+  email: string
+  product_name: string
+  product_images: string
+  product_price: number
+  category: string
+  product_description: string
+}
+
 @Injectable()
 export class MiscService {
   constructor(
     private faqRepo: FaqRepository,
     private locationRepo: LocationRepository,
-    private csvParser: CsvParser
+    private csvParser: CsvParser,
+    private userRepo: UserRepository,
+    private sellerRepo: SellerAttributeRepository,
+    private productRepo: ProductRepository,
+    private categoryRepo: CategoryRepository
   ) {}
 
   async parseUser(file: Buffer): Promise<ResponseBody<any[]>> {
-    // const stream = fs.createReadStream(file)
-    const entities = await this.csvParser.parse(file, UserParsed)
-    // console.log(entities.list)
+    const stream = bufferToStream(file)
+    const entities = await this.csvParser.parse(stream, UserParsed, null, null, { strict: true, separator: ',' })
+    for (let user of entities.list) {
+      const encrypted = await bcrypt.hash(user.email + '_' + user.phone, SALT_ROUNDS)
+      let newUser: UserEntity = {
+        id: nanoid(11),
+        full_name: user.full_name,
+        email: user.full_name,
+        phone: user.phone,
+        role: 'buyer',
+        password: encrypted,
+        created_at: new Date(),
+        updated_at: null,
+        is_active: true,
+      }
+      await this.userRepo.insert(newUser)
+    }
+    return new ResponseBody(entities.list)
+  }
+
+  async parseSeller(file: Buffer): Promise<ResponseBody<any[]>> {
+    const stream = bufferToStream(file)
+    const entities = await this.csvParser.parse(stream, SellerParsed, null, null, { strict: true, separator: ',' })
+    for (let seller of entities.list) {
+      const user = await this.userRepo.findOneOrFail({ where: { email: seller.email } })
+      let newSeller: SellerAttribute = {
+        id: nanoid(11),
+        user: user,
+        tier: 1,
+        description: seller.description,
+        shop_name: seller.shop_name,
+        shop_address: seller.shop_address,
+        birth_date: seller.birth_date,
+        birth_place: seller.birth_place,
+        gender: seller.gender,
+        image: seller.image,
+        created_at: new Date(),
+        updated_at: null,
+        is_active: true,
+      }
+      await this.sellerRepo.insert(newSeller)
+    }
+    return new ResponseBody(entities.list)
+  }
+
+  async parseProduct(file: Buffer): Promise<ResponseBody<any[]>> {
+    const stream = bufferToStream(file)
+    const entities = await this.csvParser.parse(stream, ProductParsed, 3, null, { strict: true, separator: ';' })
+    for (let product of entities.list) {
+      const user = await this.userRepo.findOneOrFail({ where: { email: product.email } })
+      const seller = await this.sellerRepo.findOneOrFail({
+        relations: ['user'],
+        where: { user: user.id }
+      })
+      const category = await this.categoryRepo.findOneOrFail(product.category)
+      let newProduct: ProductEntity = {
+        id: nanoid(11),
+        category: category,
+        seller: seller,
+        name: product.product_name,
+        price_per_quantity: product.product_price,
+        discount: 0,
+        description: product.product_description,
+        images: product.product_images.split(','),
+        available: true,
+        created_at: new Date(),
+        updated_at: null,
+        deleted_at: null
+      }
+      await this.productRepo.insert(newProduct)
+    }
+    console.log(await this.productRepo.find())
     return new ResponseBody(entities.list)
   }
 
--- test/misc.e2e-spec.ts
+++ test/misc.e2e-spec.ts
@@ -6,6 +6,17 @@ import { getConnection, getRepository } from 'typeorm';
 import { FaqEntity } from '../src/misc/entities/faq.entity';
 import { MiscModule } from '../src/misc/misc.module';
 import { LocationEntity } from '../src/misc/entities/location.entity';
+import { CategoryEntity } from '../src/products/entities/category.entity';
+import { UserEntity } from '../src/users/entities/users.entity';
+import { SellerAttribute } from '../src/users/entities/seller.entity';
+import { ProductEntity } from '../src/products/entities/product.entity';
+import { DiscussionEntity } from '../src/discussion/entities/discussion.entity';
+import { AgendaEntity } from '../src/agenda/entities/agenda.entity';
+import { ProposalEntity } from '../src/proposal/entities/proposal.entity';
+import { ProposalData } from '../src/proposal/entities/proposal-data.entity';
+import { OrderEntity } from '../src/order/entities/order.entity';
+import { OrderItem } from '../src/order/entities/order-item.entity';
+import { CartEntity } from '../src/order/entities/cart.entity';
 
 describe('GET Product and Categories (e2e)', () => {
   let app: INestApplication;
@@ -21,7 +32,18 @@ describe('GET Product and Categories (e2e)', () => {
           synchronize: true,
           entities: [
             FaqEntity,
-            LocationEntity
+            LocationEntity,
+            CategoryEntity, 
+            UserEntity, 
+            SellerAttribute, 
+            ProductEntity,
+            DiscussionEntity,
+            AgendaEntity,
+            ProposalEntity,
+            ProposalData,
+            OrderEntity,
+            OrderItem,
+            CartEntity
           ],
         }),
       ],
@@ -87,6 +109,182 @@ describe('GET Product and Categories (e2e)', () => {
   afterEach(async () => {
     await getConnection().close();
   });
+
+  // it(`should parse user and insert it`, () => {
+  //   return request(app.getHttpServer())
+  //     .post('/bulk-upload/user')
+  //     .set('Content-Type', 'multipart/form-data')
+  //     .attach('file', '../Downloads/user_list.csv')
+  //     .expect(201)
+  //     .expect({
+  //       message: 'ok',
+  //       result: [
+  //         {
+  //           full_name: 'Amiruddin',
+  //           email: 'waroeng.umkm@gmail.com',
+  //           phone: '00111232132'
+  //         },
+  //         { full_name: 'zzzz', email: 'nuril98@gmail.com', phone: '0000' }
+  //       ]
+  //     });
+  // });
+
+  // it('should parse seller and insert it', async () => {
+  //   getConnection()
+  //     .getRepository(UserEntity)
+  //     .insert([{
+  //       id: 'user-1',
+  //       full_name: "Amiruddin",
+  //       email: "waroeng.umkm@gmail.com",
+  //       phone: "081238192312",
+  //       password: 'password',
+  //       role: 'seller'
+  //     },
+  //     {
+  //       id: 'user-2',
+  //       full_name: "zzzz",
+  //       email: "nuril98@gmail.com",
+  //       phone: "0000",
+  //       password: 'password',
+  //       role: 'seller'
+  //     }])
+  //     return request(app.getHttpServer())
+  //         .post('/bulk-upload/seller')
+  //         .set('Content-Type', 'multipart/form-data')
+  //         .attach('file', '../Downloads/shop_list.csv')
+  //         .expect(201)
+  //         .expect({
+  //           message: 'ok',
+  //           result: [
+  //             {
+  //               email: 'waroeng.umkm@gmail.com',
+  //               shop_name: 'toko jaya raya',
+  //               shop_address: 'Makasar',
+  //               description: 'toko yang menjual perabotan dan mainan anak',
+  //               birth_date: '19/12/2018',
+  //               birth_place: 'Jakarta',
+  //               gender: 'Perempuan',
+  //               image: 'https://www.kindpng.com/picc/b/24/248253.png'
+  //             },
+  //             {
+  //               email: 'nuril98@gmail.com',
+  //               shop_name: 'Toko cihuy',
+  //               shop_address: 'Jatinegara',
+  //               description: 'toko menjual barang2 cihuy',
+  //               birth_date: '18/12/2008',
+  //               birth_place: 'Jakarta',
+  //               gender: 'Perempuan',
+  //               image: 'https://www.kindpng.com/picc/b/24/248253.png'
+  //             }
+  //           ]
+  //         });
+  // })
+
+  // it(`should parse product and insert it`, () => {
+  //   getConnection()
+  //     .getRepository(UserEntity)
+  //     .insert([{
+  //       id: 'user-1',
+  //       full_name: "Amiruddin",
+  //       email: "waroeng.umkm@gmail.com",
+  //       phone: "081238192312",
+  //       password: 'password',
+  //       role: 'seller'
+  //     },
+  //     {
+  //       id: 'user-2',
+  //       full_name: "zzzz",
+  //       email: "nuril98@gmail.com",
+  //       phone: "0000",
+  //       password: 'password',
+  //       role: 'seller'
+  //     }])
+  //   getConnection()
+  //     .getRepository(SellerAttribute)
+  //     .insert([{
+  //       id: 'seller-1',
+  //       user: {
+  //         id: 'user-1',
+  //         full_name: "Amiruddin",
+  //         email: "waroeng.umkm@gmail.com",
+  //         phone: "081238192312",
+  //         password: 'password',
+  //         role: 'seller'
+  //       },
+  //       tier: 1,
+  //       description: 'wawawawa',
+  //       shop_name: 'awawawa',
+  //       shop_address: 'awawaw',
+  //       birth_date: '01/02/1999',
+  //       birth_place: 'dantooine',
+  //       gender: 'lucky laki',
+  //       image: 'img-1.com'
+  //     },
+  //     {
+  //       id: 'seller-2',
+  //       user: {
+  //         id: 'user-2',
+  //         full_name: "zzzz",
+  //         email: "nuril98@gmail.com",
+  //         phone: "0000",
+  //         password: 'password',
+  //         role: 'seller'
+  //       },
+  //       tier: 1,
+  //       description: 'wawawawa',
+  //       shop_name: 'awawawa',
+  //       shop_address: 'awawaw',
+  //       birth_date: '01/02/1999',
+  //       birth_place: 'dantooine',
+  //       gender: 'lucky laki',
+  //       image: 'img-2.com'
+  //     }])
+  //     getConnection()
+  //     .getRepository(CategoryEntity)
+  //     .insert([{
+  //       id: "category-2",
+  //       name: "Minuman",
+  //       image: "minuman.jpg"
+  //     },{
+  //       id: "category-4",
+  //       name: "Pakaian",
+  //       image: "no_image"
+  //     }])
+  //   return request(app.getHttpServer())
+  //     .post('/bulk-upload/product')
+  //     .set('Content-Type', 'multipart/form-data')
+  //     .attach('file', '../Downloads/product_list.csv')
+  //     .expect(201)
+  //     .expect({
+  //       message: 'ok',
+  //       result: [
+  //         {
+  //           email: 'waroeng.umkm@gmail.com',
+  //           product_name: 'Peyek Teri',
+  //           product_images: 'https://www.noupe.com/wp-content/uploads/2016/09/basketball_shot-666x418.jpg, https://www.noupe.com/wp-content/uploads/2016/09/basketball_shot-666x418.jpg',
+  //           product_price: '15000',
+  //           category: 'category-4',
+  //           product_description: 'testing only'
+  //         },
+  //         {
+  //           email: 'nuril98@gmail.com',
+  //           product_name: 'pakaian',
+  //           product_images: 'https://www.noupe.com/wp-content/uploads/2016/09/basketball_shot-666x418.jpg',
+  //           product_price: '899999',
+  //           category: 'category-2',
+  //           product_description: 'asiiik'
+  //         },
+  //         {
+  //           email: 'waroeng.umkm@gmail.com',
+  //           product_name: 'Peyek udang',
+  //           product_images: 'https://www.noupe.com/wp-content/uploads/2016/09/basketball_shot-666x418.jpg',
+  //           product_price: '16000',
+  //           category: 'category-4',
+  //           product_description: 'testing only2'
+  //         }
+  //       ]  
+  //     });
+  // });
 
   it(`should list provinces`, () => {
     return request(app.getHttpServer())
