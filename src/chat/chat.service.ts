f9b60c6cfa4390a4589b3e4ed4c44a9c4b6a9700
fariqodri <arfixfari@gmail.com>
2020-08-17 10:52:09 +0700

    get chat rooms and chats in room

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { nanoid } from 'nanoid';
 import admin from 'firebase-admin';
 
@@ -16,14 +16,14 @@ import { ShopService } from '../shop/shop.service';
 
 @Injectable()
 export class ChatService {
-  private readonly firebaseApp: admin.app.App
+  private readonly firebaseApp: admin.app.App;
 
   constructor(
     private readonly orderService: OrderService,
     private readonly userService: UsersService,
     private readonly chatRoomRepo: ChatRoomRepository,
     private readonly chatRepo: ChatRepository,
-    private readonly shopService: ShopService
+    private readonly shopService: ShopService,
   ) {
     this.firebaseApp = admin.initializeApp({
       credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
@@ -31,14 +31,13 @@ export class ChatService {
     });
   }
 
-  async chat(
-    body: ChatDto,
-    senderId: string,
-    senderRole: 'seller' | 'buyer',
-  ): Promise<void> {
+  async chat(body: ChatDto, senderId: string, senderRole: 'seller' | 'buyer') {
     const sender = await this.userService.findUserById(senderId);
     const receiver = await this.userService.findUserById(body.receiver_user_id);
-    const seller = senderRole === 'seller' ? await this.shopService.getShopByUserId(sender.id) : await this.shopService.getShopByUserId(body.receiver_user_id)
+    const seller =
+      senderRole === 'seller'
+        ? await this.shopService.getShopByUserId(sender.id)
+        : await this.shopService.getShopByUserId(body.receiver_user_id);
     const receiverDeviceToken = receiver.device_token;
     const { chat_room_id, date, time, text, order_id } = body;
 
@@ -46,8 +45,9 @@ export class ChatService {
     if (order_id) {
       order = await this.orderService.findOrderById(order_id);
     }
+    const chat_id = nanoid(11);
     let chat: Partial<ChatEntity> = {
-      id: nanoid(11),
+      id: chat_id,
       date: date,
       time: time,
       text: text,
@@ -55,21 +55,25 @@ export class ChatService {
       sender: sender,
       receiver: receiver,
     };
-
+    let room_id: string;
     if (chat_room_id) {
       try {
+        room_id = chat_room_id;
         const room = await this.chatRoomRepo.findOneOrFail(chat_room_id);
         chat = {
           ...chat,
           room: room,
         };
         await this.chatRepo.insert(chat);
-      } catch(err) {
-        throw new ConflictException(new ResponseBody(null, 'chat room has not been created'))
+      } catch (err) {
+        throw new ConflictException(
+          new ResponseBody(null, 'chat room has not been created'),
+        );
       }
     } else {
+      room_id = nanoid(11);
       const room = this.chatRoomRepo.create({
-        id: nanoid(11),
+        id: room_id,
         buyer: sender,
         seller: receiver,
       });
@@ -80,13 +84,15 @@ export class ChatService {
       };
       await this.chatRepo.insert(chat);
     }
-    
+
     if (receiverDeviceToken) {
-      let notificationBody: string
+      let notificationBody: string;
       if (text) {
-        notificationBody = text
+        notificationBody = text;
       } else if (order_id) {
-        notificationBody =  `${senderRole === 'seller' ? seller.shop_name: sender.full_name} mengirim sebuah pesan`
+        notificationBody = `${
+          senderRole === 'seller' ? seller.shop_name : sender.full_name
+        } mengirim sebuah pesan`;
       }
       await this.firebaseApp.messaging().sendToDevice(
         receiverDeviceToken,
@@ -100,15 +106,67 @@ export class ChatService {
             time: chat.time,
           },
           notification: {
-            title: `Pesan dari ${senderRole === 'seller' ? seller.shop_name : sender.full_name}`,
+            title: `Pesan dari ${
+              senderRole === 'seller' ? seller.shop_name : sender.full_name
+            }`,
             body: notificationBody,
-            clickAction: "FLUTTER_NOTIFICATION_CLICK"
-          }
+            clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+          },
         },
         {
           collapseKey: 'Waroenk UMKM',
         },
       );
     }
+    return {
+      chat_room_id: room_id,
+      chat_id: chat_id,
+      text: body.text,
+      order_id: body.order_id,
+      date: body.date,
+      time: body.time,
+    };
+  }
+
+  async getChatRoomsByUserId(userId: string) {
+    return this.chatRoomRepo
+      .createQueryBuilder('room')
+      .innerJoin('room.buyer', 'buyer')
+      .innerJoin('room.seller', 'seller')
+      .where('buyer.id = :id OR seller.id = :id', { id: userId })
+      .select([
+        'room.id',
+        'buyer.id',
+        'buyer.role',
+        'buyer.full_name',
+        'seller.id',
+        'seller.role',
+        'seller.full_name',
+      ])
+      .getMany();
+  }
+
+  async getChatsInRoom(roomId: string, userId: string) {
+    const res = await this.chatRepo
+      .createQueryBuilder('chat')
+      .innerJoin('chat.room', 'room')
+      .innerJoin('chat.sender', 'sender')
+      .innerJoin('chat.receiver', 'receiver')
+      .where('room.id = :roomId', { roomId })
+      .select([
+        'chat.id',
+        'room.id',
+        'sender.id',
+        'sender.full_name',
+        'receiver.id',
+        'receiver.full_name',
+        'chat.text',
+        'chat.order.id',
+      ])
+      .getMany();
+    return res.map(r => ({
+      ...r,
+      sent_by_me: r.sender.id === userId,
+    }));
   }
 }
