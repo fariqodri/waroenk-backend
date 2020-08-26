4b265b97f5ce3a139a44fcb17c3920253d62a062
kemas.khaidar <kemask69@gmail.com>
2020-08-26 12:32:15 +0700

    add isSellerProduct in discussion

--- src/discussion/discussion.module.ts
+++ src/discussion/discussion.module.ts
@@ -5,9 +5,14 @@ import { DiscussionRepository } from './repositories/discussion.repository';
 import { DiscussionService } from './services/discussion.service';
 import { UserRepository } from '../users/repositories/users.repository';
 import { ProductRepository } from '../products/repositories/product.repository';
+import { SellerAttributeRepository } from '../users/repositories/seller.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository, ProductRepository, DiscussionRepository])],
+  imports: [TypeOrmModule.forFeature([
+    UserRepository,
+    ProductRepository, 
+    DiscussionRepository,
+    SellerAttributeRepository])],
   controllers: [DiscussionController],
   providers: [DiscussionService],
   exports: [TypeOrmModule.forFeature([DiscussionRepository])]
--- src/discussion/services/discussion.service.spec.ts
+++ src/discussion/services/discussion.service.spec.ts
@@ -3,13 +3,19 @@ import { DiscussionService } from './discussion.service';
 import { DiscussionRepository } from '../repositories/discussion.repository';
 import { UserRepository } from '../../users/repositories/users.repository';
 import { ProductRepository } from '../../products/repositories/product.repository';
+import { SellerAttributeRepository } from '../../users/repositories/seller.repository';
 
 describe('DiscussionService', () => {
   let service: DiscussionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DiscussionService, UserRepository, DiscussionRepository, ProductRepository],
+      providers: [
+        DiscussionService, 
+        UserRepository, 
+        DiscussionRepository, 
+        ProductRepository,
+        SellerAttributeRepository],
     }).compile();
 
     service = module.get<DiscussionService>(DiscussionService);
