2e7031ece8858cb284830c86610c9c29475c7048
kemas.khaidar <kemask69@gmail.com>
2020-09-27 21:48:05 +0700

    add sort_by list discussion

--- src/discussion/services/discussion.service.ts
+++ src/discussion/services/discussion.service.ts
@@ -99,7 +99,7 @@ export class DiscussionService {
       query = query.andWhere('LOWER(d.description) LIKE :search', { search: `%${param.search}%` })
     }
     if (param.sort_by && param.sort_by !== "") {
-      query = query.orderBy(d.created_at,
+      query = query.orderBy('d.created_at',
         param.sort_by == "newest"? "DESC": "ASC")
     }
     let discussions = await query.execute()
