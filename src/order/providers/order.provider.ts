e206d510c78f4eb1b892a017ed9125fa48d1823f
fariqodri <arfixfari@gmail.com>
2020-10-05 21:39:42 +0700

    add chat order title

--- src/order/providers/order.provider.ts
+++ src/order/providers/order.provider.ts
@@ -1,14 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { FindOneOptions } from "typeorm";
 import { OrderEntity } from "../entities/order.entity";
+import { OrderItemRepository } from "../repositories/order-item.repository";
 import { OrderRepository } from "../repositories/order.repository";
 
 @Injectable()
 export class OrderProvider {
   constructor(
-    private readonly orderRepo: OrderRepository
+    private readonly orderRepo: OrderRepository,
+    private readonly orderItemRepo: OrderItemRepository
   ) {}
   findOrderById(orderId: string, option?: FindOneOptions<OrderEntity>): Promise<OrderEntity> {
     return this.orderRepo.findOneOrFail(orderId, option)
+  }
+
+  countProductsOfOrder(orderId: string): Promise<number> {
+    return this.orderItemRepo.count({
+      where: {
+        order: {
+          id: orderId
+        }
+      }
+    })
   }
 }
