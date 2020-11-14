806e03a492e3cd102b7fef8c270f77d63ea14c33
Fari Qodri Andana <arfixfari@gmail.com>
2020-11-14 14:59:39 +0000

    limit file type

--- src/file.upload/file.upload.controller.ts
+++ src/file.upload/file.upload.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, HttpCode, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
+import { Controller, Post, HttpCode, UseInterceptors, UploadedFile, UseGuards, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express'
 import { FileUploadService } from './file.upload.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import * as path from 'path'
+import { ResponseBody } from '../utils/response';
 
 @Controller('file')
 export class FileUploadController {
@@ -10,7 +12,16 @@ export class FileUploadController {
   @Post()
   @HttpCode(200)
   @UseGuards(JwtAuthGuard)
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: (req, file, cb) => {
+      const ext = path.extname(file.originalname)
+      const allowedExtensions = ['.png', '.jpeg', '.jpg', '.pdf', '.csv']
+      if (!allowedExtensions.includes(ext)) {
+        return cb(new BadRequestException(new ResponseBody(null, 'file type not allowed')), false)
+      }
+      return cb(null, true)
+    }
+  }))
   uploadFile(@UploadedFile() file) {
     return this.service.uploadFile(file.buffer, file.mimetype, file.originalname)
   }
